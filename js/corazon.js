39b98b8a9043d87d7cad93257f38bb8e36a6f07e
MIKE RM <mikepruebas23@gmail.com>
2019-12-09 14:39:50 -0700

    Re-organizacion del proyecto y añadiendo interfaz de puntos.

---
+++ js/corazon.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function () {
+   
+    
+});
+
+const $like = document.getElementById('like');
+const $rayo = document.getElementById('rayo');
+var contador = 12;
+
+
+function repetir(){
+    // $like.classList.toggle('is-liked');
+    // $rayo.classList.toggle('is-rayo');
+}
+
+var timer = 1500;
+setInterval('repetir()',timer);
